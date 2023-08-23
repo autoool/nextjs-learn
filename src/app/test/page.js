f468de4f43565ca7e7738a0ad9ad159a13e3a399
chaozhang <chaozaiai@163.com>
2023-08-23 23:43:17 +0800

    1

---
+++ src/app/test/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../fontawesome.min.css'
+
+function page() {
+  return (
+    <div>
+      <i className="fa fa-car font-size-48"></i>
+      <i className="fa fa-car" ></i>
+      <i className="fa fa-car" ></i>
+    </div>
+  )
+}
+
+export default page
